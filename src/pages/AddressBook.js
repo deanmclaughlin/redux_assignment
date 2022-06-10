368303a3029a766d087bea0d3cd4b538e9108288
deanmclaughlin <106060148+deanmclaughlin@users.noreply.github.com>
2022-06-09 20:52:05 -0600

    Add files via upload

--- src/pages/AddressBook.js
+++ src/pages/AddressBook.js
@@ -7,7 +7,7 @@ class AddressBook extends React.Component {
                 <h2>Address Book</h2>
                 <button onClick={() => {this.props.onClear();}}>DELETE ALL</button>
 				<br/><br/>
-				<ul>{this.props.items.map(x => <li key={x}>NAME:&nbsp; {x[0]} {<br/>} PHONE: {x[1]}</li>)}</ul>
+				<ul>{this.props.items.map(x => <li key={x}>NAME:&nbsp; {x[0]} {<br/>} PHONE:&nbsp; {x[1]}</li>)}</ul>
             </div>
         );
     }
